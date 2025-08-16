dd49ebd29b9e4076044cf04b598e581a1b598d2b
MSTY-Enterprises <mstyenterprisesnsolutions@gmail.com>
2025-08-16 11:11:20 +0530

    test6

--- app/api/emails/message/route.js
+++ app/api/emails/message/route.js
@@ -1,6 +1,6 @@
 "use server";
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@/app/generated/prisma/client.js";
+import { PrismaClient } from "@prisma/client";
 export async function GET() {
   const prisma = new PrismaClient(); 
   try {
